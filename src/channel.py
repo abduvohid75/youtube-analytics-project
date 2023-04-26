45d20c10581d22e8e7af58ecd67a307b9672b900
abduvohid75 <rustam75@list,ru>
2023-04-26 09:46:13 +0500

    api

--- src/channel.py
+++ src/channel.py
@@ -1,16 +1,28 @@
+import json
 import os
 import requests
 from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('API_KEY_YOU_TUBE')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'
 
     def __init__(self, channel_id: str) -> None:
-        self.channel_id = channel_id
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
 
+        self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
+        self.title = self.channel['title']
 
     def print_info(self) -> None:
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        return print(channel)
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
+        print(channel_id)
+
+    @classmethod
+    def get_service(cls):
+        return
+    def to_json(dict_to_print: dict) -> None:
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
