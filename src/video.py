4a792b298c3bb855daa327907bef427d9679bbce
abduvohid75 <rustam75@list,ru>
2023-05-22 11:24:23 +0500

    api

--- src/video.py
+++ src/video.py
@@ -3,17 +3,34 @@ import os
 import pickle
 import requests
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 class Video:
     api_key: str = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
-
     def __init__(self, video_id: str) -> None:
         self.video_id = video_id
         self.video = self.youtube.videos().list(id=self.video_id, part='snippet, statistics').execute()
-        self.title = self.video['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch/{self.video['items'][0]['id']}"
-        self.view_count = self.video['items'][0]['statistics']['viewCount']
-        self.likeCount = self.video['items'][0]['statistics']['likeCount']
+        try:
+            self.title = self.video['items'][0]['snippet']['title']
+        except IndexError:
+            self.title = None
+            print(1)
+
+        try:
+            self.url = f"https://www.youtube.com/watch/{self.video['items'][0]['id']}"
+        except IndexError:
+            print(2)
+        try:
+            self.view_count = self.video['items'][0]['statistics']['viewCount']
+        except:
+            self.view_count = None
+            print(3)
+        try:
+            self.likeCount = self.video['items'][0]['statistics']['likeCount']
+        except:
+            self.likeCount = None
+            print(4)
+
 class PLVideo(Video):
     def __init__(self, video_id, channel_id):
         super().__init__(video_id)
